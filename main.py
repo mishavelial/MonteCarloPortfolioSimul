8e9b88464e0616d2410ff3a0a8c0984010f2b702
mishavelial <mikhailandropov@icloud.com>
2025-12-26 23:11:38 +0100

    Cleaning.2 - a Pt,i matrix is inplace where t is time and i is the asset. Ive chose Apple, Microsoft and S&P500.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import pandas as pd
 df = yf.download(tickers, start='2020-01-01', end='2025-12-23')
 
 df.to_csv('data.csv')
-"""
 df = pd.read_csv('data.csv')
 
 #deleting irrelevant columns, just for now
@@ -15,4 +14,12 @@ df.drop(columns=items, inplace=True)
 df.drop(index=1, inplace=True)
 df.reset_index(drop=True, inplace=True)
 df.to_csv('data.csv')
+
+df.columns = df.iloc[0]
+df = df[1:].reset_index(drop=True)
+df.set_index('Ticker', inplace=True)
+df.to_csv('data.csv')"""
+
+df = pd.read_csv('data.csv')
+
 print(df.head())
