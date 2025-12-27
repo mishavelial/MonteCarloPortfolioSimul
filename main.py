83caba6eaca11fde1f3635e31d5da91db9729632
mishavelial <mikhailandropov@icloud.com>
2025-12-27 02:18:06 +0100

    number of paths and the mc simulation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
 #when used for the first time, uncomment the code below
 """tickers = ['AAPL', 'MSFT', 'SPY']
@@ -31,9 +32,20 @@ df['S&P500 DR'] = np.log(df['SPY']).diff().dropna()
 
 #mean and covariance
 assets = ['Apple DR', 'Microsoft DR', 'S&P500 DR']
-
 mu  = df[assets].mean()
 cov = df[assets].cov()
 
-print(mu)
-print(cov)
+#we add the weights of the assets out of our portfolio and the initial capital we are planning to start with. say our portfolio is 10k, out of that 4k is in apple, 3k in microsoft and the rest in in s&p
+weighs = np.array([0.4, 0.3, 0.3])
+initial_capital = 10_000
+portfolio_value = initial_capital * (1 + df[assets]).cumprod()
+
+#number of paths and the mc simulation
+n_simulations = 10_000
+
+sim_asset_returns = np.random.multivariate_normal(mu, cov, n_simulations)
+
+wealth_curve = initial_capital * (1 + sim_asset_returns).cumprod()
+
+plt.plot(wealth_curve, label='Wealth')
+plt.show()
